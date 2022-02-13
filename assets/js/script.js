eccb58a08983032013ae649f1216637d930649c1
aidyel <aidayeli@icloud.com>
2022-02-12 19:39:14 -0500

    added submission form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,16 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#task-to-do");
 
-var createElementTaskHandler = function() {
+var createTaskHandler = function(event) {
+    event.preventDefault();
+
+
     var listItemEl =document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
-}
+ 
+};
 
-buttonEl.addEventListener("click", createElementTaskHandler); 
+formEl.addEventListener("submit", createTaskHandler); 
 
