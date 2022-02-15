bd5ad4dfa78702bd6885e9dde785838f83a844e8
aidyel <aidayeli@icloud.com>
2022-02-15 09:38:12 -0500

    form-submit

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,18 +3,28 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var taskFormHandler = function(event) {
     event.preventDefault();
-
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
    
+   // check if inputs are empty (validate)
+     if (!taskNameInput || !taskTypeInput) {
+         alert("You need to fill out the task form!");
+         return false;
+   }
+
+   formEl.reset();
+
+   // reset form fields for next task to be entered
+  document.querySelector("input[name='task-name']").value = "";
+  document.querySelector("select[name='task-type']").selectedIndex = 0;
+
   // package up data as an object
     var taskDataObj = {
         name: taskNameInput,
         type: taskTypeInput
     };
-    // send it as an argument to createTaskEl
-     createTaskEl(taskDataObj);
-}
+    createTaskEl(taskDataObj);
+};
 
 var createTaskEl = function(taskDataObj) {
 
@@ -29,6 +39,8 @@ var createTaskEl = function(taskDataObj) {
     taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type' >" + taskDataObj.type + "</span>";
     listItemEl.appendChild(taskInfoEl);
 
+    console.dir(listItemEl);
+    
     // add entire list item to list
     tasksToDoEl.appendChild(listItemEl);
 }
